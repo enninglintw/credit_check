29b0bae95a3be9ff636780d933cb3ca842a7413c
playpool513 <playpool513@gmail.com>
2017-07-13 20:58:01 +1000

    Add method double_every_other_digit

--- credit_check.rb
+++ credit_check.rb
@@ -2,4 +2,17 @@ class CreditCheck
   def initialize(credit)
     @credit = credit
   end
+
+  def double_every_other_digit
+    digits = @credit.split("")
+    check_digit_index = digits.count - 1
+
+    digits.each_with_index.map do |digit, index|
+      (check_digit_index - index).odd? ? digit.to_i * 2 : digit.to_i
+    end
+  end
+
+  def valid?
+
+  end
 end
