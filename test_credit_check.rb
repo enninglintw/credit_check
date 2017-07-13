de52dc3c7adc8483a3529c81c0964be80de9d4b8
playpool513 <playpool513@gmail.com>
2017-07-13 20:57:11 +1000

    Add test for double_every_other_digit

--- test_credit_check.rb
+++ test_credit_check.rb
@@ -6,6 +6,10 @@ class TestCreditCheck < Minitest::Test
     @credit_check = CreditCheck.new("4929735477250543")
   end
 
+  def test_double_every_other_digit
+    assert_equal [8, 9, 4, 9, 14, 3, 10, 4, 14, 7, 4, 5, 0, 5, 8, 3], @credit_check.double_every_other_digit
+  end
+
   def test_valid
     assert_equal true, @credit_check.valid?
   end
