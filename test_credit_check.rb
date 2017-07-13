f3394f73c8539578802ebd35b69753c6379e3e5c
playpool513 <playpool513@gmail.com>
2017-07-13 20:59:25 +1000

    Add test for sum_digits_over_nine

--- test_credit_check.rb
+++ test_credit_check.rb
@@ -10,6 +10,10 @@ class TestCreditCheck < Minitest::Test
     assert_equal [8, 9, 4, 9, 14, 3, 10, 4, 14, 7, 4, 5, 0, 5, 8, 3], @credit_check.double_every_other_digit
   end
 
+  def test_sum_digits_over_nine
+    assert_equal [8, 9, 4, 9, 5, 3, 1, 4, 5, 7, 4, 5, 0, 5, 8, 3], @credit_check.sum_digits_over_nine
+  end
+
   def test_valid
     assert_equal true, @credit_check.valid?
   end
